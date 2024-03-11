9bd7360a45178cab6cc0c3239ebfc77a12d75d5e
Daniel Contreras <danicontreras0409@gmail.com>
2024-03-11 09:36:32 -0600

    Cambiar rutas raíz a /vis

--- lab17/controllers/crear.controller.js
+++ lab17/controllers/crear.controller.js
@@ -12,9 +12,10 @@ exports.post_armar = (request, response, next) => {
 
     const artista = 
         new Artista(request.body.nombre, request.body.imagen, request.body.descripcion);
+        
     artista.save().then(([rows, fieldData]) => {
         response.setHeader('Set-Cookie', 'ultimo_artista =' + request.body.nombre + ';HttpOnly');
-        response.redirect('/');
+        response.redirect('/vis');
     })
     .catch(err => {
         console.log(err);
