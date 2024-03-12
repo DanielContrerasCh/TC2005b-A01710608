ff95c90738c483f083916284da85c0d38650c4e8
Daniel Contreras <danicontreras0409@gmail.com>
2024-03-12 09:35:13 -0600

    Proteger rutas de la aplicación

--- lab17/routes/artistas.routes.js
+++ lab17/routes/artistas.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth');
 
 const artistasController = require('../controllers/artistas.controller');
 
-router.use('/artistas/1', artistasController.get_artista1);
+router.use('/artistas/1', isAuth, artistasController.get_artista1);
 
-router.use('/artistas/2', artistasController.get_artista2);
+router.use('/artistas/2', isAuth, artistasController.get_artista2);
 
-router.use('/artistas/3', artistasController.get_artista3);
+router.use('/artistas/3', isAuth, artistasController.get_artista3);
 
 module.exports = router;
--- lab17/routes/crear.routes.js
+++ lab17/routes/crear.routes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth');
 
 const crearController = require('../controllers/crear.controller');
 
-router.get('/vis', crearController.get_list);
+router.get('/vis', isAuth, crearController.get_list);
 
-router.get('/armar', crearController.get_armar);
+router.get('/armar', isAuth, crearController.get_armar);
 
-router.post('/armar', crearController.post_armar);
+router.post('/armar', isAuth, crearController.post_armar);
 
-router.get('/vis/:artistaCreado_id', crearController.get_list);
+router.get('/vis/:artistaCreado_id', isAuth, crearController.get_list);
 
-router.get('/', crearController.get_root);
+router.get('/', isAuth, crearController.get_root);
 
 module.exports = router;
