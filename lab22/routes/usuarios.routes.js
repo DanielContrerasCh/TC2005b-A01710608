ddf1be4bf22ac89e20c1e7da07797153655f0c1a
Daniel Contreras <danicontreras0409@gmail.com>
2024-04-01 09:20:34 -0600

    Creación de carpeta de lab22

---
+++ lab22/routes/usuarios.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const canView = require('../util/can-view');
+const canCreate = require('../util/can-create');
+
+const usuariosController = require('../controllers/usuarios.controller');
+
+router.get('/login', usuariosController.getLogin);
+
+router.post('/login', usuariosController.postLogin);
+
+router.get('/logout', usuariosController.getLogOut);
+
+router.get('/signUp', usuariosController.getSignUp);
+
+router.post('/signUp', usuariosController.postSignUp);
+
+module.exports = router;
